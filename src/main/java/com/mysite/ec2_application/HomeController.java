5cdbe3b8bb3c1f87ef0ab746b6cd08cee8c7933a
k-haechan <gibbm1127@naver.com>
2025-04-22 12:31:57 +0900

    s3

---
+++ src/main/java/com/mysite/ec2_application/HomeController.java
@@ -0,0 +1,43 @@
+package com.mysite.ec2_application;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.Bucket;
+
+/**
+ * PackageName : com.mysite.ec2_application
+ * FileName    : HomeController
+ * Author      : hc
+ * Date        : 25. 4. 22.
+ * Description : 
+ * =====================================================================================================================
+ * DATE          AUTHOR               NOTE
+ * ---------------------------------------------------------------------------------------------------------------------
+ * 25. 4. 22.     hc               Initial creation
+ */
+@RestController
+@RequiredArgsConstructor
+public class HomeController {
+	private final S3Client s3Client;
+
+	@GetMapping("/")
+	public String home() {
+		return "Hello World!";
+	}
+
+	@GetMapping("/buckets")
+	public List<String> listAllBuckets() {
+		return s3Client
+			.listBuckets()
+			.buckets()
+			.stream()
+			.map(Bucket::name)
+			.collect(Collectors.toList());
+	}
+}
