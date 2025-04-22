122469ed54998651d56e4a05c48bc8367b1e4c79
k-haechan <gibbm1127@naver.com>
2025-04-22 18:48:50 +0900

    s3 의존성 제거

--- src/main/java/com/mysite/ec2_application/HomeController.java
+++ src/main/java/com/mysite/ec2_application/HomeController.java
@@ -1,14 +1,9 @@
 package com.mysite.ec2_application;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
-import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.Bucket;
 
 /**
  * PackageName : com.mysite.ec2_application
@@ -24,20 +19,20 @@ import software.amazon.awssdk.services.s3.model.Bucket;
 @RestController
 @RequiredArgsConstructor
 public class HomeController {
-	private final S3Client s3Client;
+	// private final S3Client s3Client;
 
 	@GetMapping("/")
 	public String home() {
 		return "Hello World!";
 	}
 
-	@GetMapping("/buckets")
-	public List<String> listAllBuckets() {
-		return s3Client
-			.listBuckets()
-			.buckets()
-			.stream()
-			.map(Bucket::name)
-			.collect(Collectors.toList());
-	}
+	// @GetMapping("/buckets")
+	// public List<String> listAllBuckets() {
+	// 	return s3Client
+	// 		.listBuckets()
+	// 		.buckets()
+	// 		.stream()
+	// 		.map(Bucket::name)
+	// 		.collect(Collectors.toList());
+	// }
 }
